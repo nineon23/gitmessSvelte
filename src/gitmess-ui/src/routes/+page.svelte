efbac49840f32b476217aaa9de89ac4a059a2272
Matteo Bortolazzo <matteobortolazzo@pm.me>
2024-05-21 19:24:37 +0200

    initial commit

---
+++ src/gitmess-ui/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="card p-4">
+	<header>
+		<h3 class="h3">Repos</h3>
+	</header>
+	<article class="mt-4">
+		<nav class="list-nav">
+			<ul>
+				<li>
+					<a href="/repos/1">
+						<span class="badge bg-primary-500">(icon)</span>
+						<span class="flex-auto">Repo</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</article>
+</div>
