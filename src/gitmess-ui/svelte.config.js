6ac900d295877afc3084983f32fc8f9a33cda4c9
Matteo Bortolazzo <matteobortolazzo@pm.me>
2024-05-22 23:40:39 +0200

    Add dockerfile for UI

--- src/gitmess-ui/svelte.config.js
+++ src/gitmess-ui/svelte.config.js
@@ -13,13 +13,7 @@ const config = {
 	},
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			fallback: 'index.html' // may differ from host to host
 		})
 	}
 };
