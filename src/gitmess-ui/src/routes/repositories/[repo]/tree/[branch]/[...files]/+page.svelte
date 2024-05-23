414d9cc4fee3bef420896aee1413424889d37c42
nineon23 <carlo03fabris@gmail.com>
2024-05-23 17:21:43 +0200

    repo api modif

--- src/gitmess-ui/src/routes/repositories/[repo]/tree/[branch]/[...files]/+page.svelte
+++ src/gitmess-ui/src/routes/repositories/[repo]/tree/[branch]/[...files]/+page.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import GitDirectory from '$lib/components/GitDirectory.svelte';
-	import { CodeBlock } from '@skeletonlabs/skeleton';
+	import {CodeBlock, ListBox, ListBoxItem} from '@skeletonlabs/skeleton';
 	import type { GetTreeResponse } from '$lib/models/api.model';
 	import { getRepositoryTree } from '$lib/api';
 	import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 
+	let branches = [];
+	let selectedBranch = '';
+
+	const getBranches = async () => {
+		// Replace this with your actual API call or other method of getting branches
+		return ['main', 'develop', 'feature'];
+	};
+
+	const handleBranchClick = (branch) => {
+		selectedBranch = branch;
+		goto(`/repositories/${$page.params.repo}/tree/${branch}`);
+	};
+
+	onMount(async () => {
+		branches = await getBranches();
+	});
 	let response: GetTreeResponse | null = null;
 
 	const loadTree = async () => {
@@ -19,12 +38,18 @@
 	page.subscribe(loadTree);
 
 	onMount(loadTree);
+
 </script>
 
 <div class="card p-4">
 	<header>
 		<h3 class="h3">{$page.params.repo}</h3>
 		<h4 class="h4">{$page.params.branch}</h4>
+		<ListBox>
+			{#each branches as branch}
+				<ListBoxItem bind:group={selectedBranch} name="branch" value={branch} on:click={() => handleBranchClick(branch)}>{branch}</ListBoxItem>
+			{/each}
+		</ListBox>
 	</header>
 
 	{#if response}
